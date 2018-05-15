b88b7cd7a0242d55f2da807f02c77979ad268e79
Daiki Kojima <daikikojima0531@icloud.com>
2018-05-15 21:58:11 +0900

    src/App.js: modify header

--- src/App.js
+++ src/App.js
@@ -27,8 +27,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Todo Apps</h1>
         </header>
         <List props={this.state} delete = {this.delete} />
         <AddTodo addTodo={this.addTodo}/>
