5497eab3250e9d26a2447ef9fd801984af0ceb7e
Daiki Kojima <daikikojima0531@icloud.com>
2018-05-15 21:57:19 +0900

    src/App.js: add List component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css'
 import AddTodo from './AddTodo'
+import List from './List'
 
 class App extends Component {
   render() {
@@ -10,9 +11,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <List props={this.state} delete = {this.delete} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
