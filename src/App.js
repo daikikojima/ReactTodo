9551bcda138e77c24d80cce5868a0a4d39b0dc05
Daiki Kojima <daikikojima0531@icloud.com>
2018-05-15 21:57:49 +0900

    src/App.js: add functions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import AddTodo from './AddTodo'
 import List from './List'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: []
+    }
+    this.addTodo = this.addTodo.bind(this)
+    this.delete = this.delete.bind(this)
+  }
+  addTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+  delete(i) {
+    this.state.todos.splice(i, 1)
+    this.setState({
+      todos: this.state.todos
+    })
+  }
   render() {
     return (
       <div className="App">
